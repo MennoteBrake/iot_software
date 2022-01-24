edf703eabf13b2c73cb6097ac04c7a264020c95c
Menno te Brake <mennoteb@hotmail.nl>
2022-01-24 11:59:44 +0100

    implemented basic rest functions using the tutorial

---
+++ pi_rest_server/server.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, abort, make_response, request
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title' : "Leer",
+        'description' : "rest api",
+        'done': False
+    },
+    {
+        'id': 2,
+        'title' : "Leer2",
+        'description' : "rest api2",
+        'done': False
+    }
+]
+
+@app.route('/todo/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/todo/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/tasks', methods=['POST'])
+def create_task():
+    if not request.json or 'title' not in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+@app.route('/todo/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error' : 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- pi_rest_server/setup_commands.md
+++ pi_rest_server/setup_commands.md
@@ -7,3 +7,20 @@ sudo apt-get install python3-venv
 ```
 python -m venv flask
 ```
+Or
+```
+python3 -m venv flask
+```
+
+To install flask:
+```
+flask/bin/pip3 install flask
+```
+
+
+# To run:
+
+```
+export FLASK_APP=server.py
+flask/bin/flask run --host=0.0.0.0
+```
