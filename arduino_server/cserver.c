a11e48b3f8c8caec3655ba8e11e4d6b91542d3e4
Menno <mennoteb@hotmail.nl>
2022-06-26 11:28:53 +0200

    clang formatted

--- arduino_server/cserver.c
+++ arduino_server/cserver.c
@@ -1,5 +1,4 @@
 #include "cserver.h"
-
 #include <stdio.h>
 
 #define MSG_SIZE 100
@@ -16,39 +15,34 @@
 //   request); return resp;
 // }
 
-struct response handleRequest(struct stream stream) {
+struct response handleRequest(struct stream stream)
+{
   printf("Characters: %c \n");
   enum parserState state = EXPECT_METHOD;
-  struct request request = {.method = METHOD_UNKNOWN,
-                            .target = TARGET_UNKNOWN};
-  struct response response = {INTERNAL_SERVER_ERROR_500,
-                              51};
+  struct request request = { .method = METHOD_UNKNOWN, .target = TARGET_UNKNOWN };
+  struct response response = { INTERNAL_SERVER_ERROR_500, 51 };
 
-  char msg[MSG_SIZE] = {0};
+  char msg[MSG_SIZE] = { 0 };
 
   int done = 0;
 
-  while (!done) {
+  while (!done)
+  {
     struct token token = getNextToken(stream);
 
 #ifndef NDEBUG
-    snprintf(msg, MSG_SIZE,
-             "* token type %d, token value (\"%s\")\n",
-             token.type, token.value);
+    snprintf(msg, MSG_SIZE, "* token type %d, token value (\"%s\")\n", token.type, token.value);
 //    logmsg/(msg);
 #endif
 
-    enum parserState next =
-        parseNextToken(token, state, &request);
+    enum parserState next = parseNextToken(token, state, &request);
 
 #ifndef NDEBUG
     snprintf(msg, MSG_SIZE, "* parser state %d\n", next);
 //    logms/g(msg);
 #endif
 
-    done =
-        state ==
-        DONE; // readyToRespond(state, &request, &response);
+    done = state == DONE;  // readyToRespond(state, &request, &response);
     state = next;
   }
 
@@ -77,42 +71,44 @@ struct response handleRequest(struct stream stream) {
 //   }
 // }
 
-struct response makeResponse(struct request* request) {
+struct response makeResponse(struct request* request)
+{
   struct response response;
-  switch (request->target) {
-  case TARGET_UNKNOWN:
-    // struct response response = { .code = NOT_FOUND_404,
-    // .get_actual = 0 }; return response;
-    break;
-  case AVG1:
-    // struct response response; // = { .code =
-    // OK_200_GET_AVG, .get_avg = 51 };
-    response.code = OK_200_GET_AVG;
-    response.get_avg = 3;
-    return response;
-    // return OK_200_GET_AVG;
-    response.code = OK_200_GET_AVG;
-    response.get_avg = 3;
-    return response;
-    break;
-  case AVG2:
-    // return OK_200_GET_AVG;
-    break;
-  case STDEV1:
-    // return OK_200_GET_STDEV;
-    break;
-  case STDEV2:
-    // return OK_200_GET_STDEV;
-    break;
-  case ACTUAL1:
-    // return OK_200_GET_ACTUAL;
-    break;
-  case ACTUAL2:
-    // return OK_200_GET_ACTUAL;
-    break;
-  default:
-    // return INTERNAL_SERVER_ERROR_500;
-    break;
+  switch (request->target)
+  {
+    case TARGET_UNKNOWN:
+      // struct response response = { .code = NOT_FOUND_404,
+      // .get_actual = 0 }; return response;
+      break;
+    case AVG1:
+      // struct response response; // = { .code =
+      // OK_200_GET_AVG, .get_avg = 51 };
+      response.code = OK_200_GET_AVG;
+      response.get_avg = 3;
+      return response;
+      // return OK_200_GET_AVG;
+      response.code = OK_200_GET_AVG;
+      response.get_avg = 3;
+      return response;
+      break;
+    case AVG2:
+      // return OK_200_GET_AVG;
+      break;
+    case STDEV1:
+      // return OK_200_GET_STDEV;
+      break;
+    case STDEV2:
+      // return OK_200_GET_STDEV;
+      break;
+    case ACTUAL1:
+      // return OK_200_GET_ACTUAL;
+      break;
+    case ACTUAL2:
+      // return OK_200_GET_ACTUAL;
+      break;
+    default:
+      // return INTERNAL_SERVER_ERROR_500;
+      break;
   }
 }
 
--- arduino_server/tokenizer.c
+++ arduino_server/tokenizer.c
@@ -1,81 +1,110 @@
 #include "tokenizer.h"
 
-struct token getNextToken(struct stream stream) {
+struct token getNextToken(struct stream stream)
+{
   // printf("getnexttoken: \n");
   struct token tok;
   memset(tok.value, '\0',
-         TOKEN_ARR_LEN); // zero all characters
+         TOKEN_ARR_LEN);  // zero all characters
 
-  if (!stream.available()) {
+  if (!stream.available())
+  {
     tok.type = UNAVAILABLE;
     return tok;
   }
 
   int n = 0;
 
-  if (stream.peek() == ' ') {
+  if (stream.peek() == ' ')
+  {
     printf("whitespace \n");
     tok.type = WS;
-    while (stream.available()) {
-      if (n == TOKEN_LEN) {
+    while (stream.available())
+    {
+      if (n == TOKEN_LEN)
+      {
         tok.type = TOO_LONG;
         return tok;
-      } else if (stream.peek() == ' ') {
+      }
+      else if (stream.peek() == ' ')
+      {
         tok.value[n] = stream.read();
         n++;
-      } else {
+      }
+      else
+      {
         return tok;
       }
     }
-  } else if (isNumber(stream.peek())) {
+  }
+  else if (isNumber(stream.peek()))
+  {
     printf("number \n");
     tok.type = NUMBER;
-    while (stream.available()) {
-      if (n == TOKEN_LEN) {
+    while (stream.available())
+    {
+      if (n == TOKEN_LEN)
+      {
         tok.type = TOO_LONG;
         return tok;
-      } else if (stream.peek() >= '0' &&
-                 stream.peek() <= '9') {
+      }
+      else if (stream.peek() >= '0' && stream.peek() <= '9')
+      {
         tok.value[n] = stream.read();
         n++;
-      } else {
+      }
+      else
+      {
         return tok;
       }
     }
-  } else if (isLetter(stream.peek()) ||
-             isOther(stream.peek())) {
+  }
+  else if (isLetter(stream.peek()) || isOther(stream.peek()))
+  {
     printf("letter \n");
     tok.type = WORD;
-    while (stream.available()) {
-      if (n == TOKEN_LEN) {
+    while (stream.available())
+    {
+      if (n == TOKEN_LEN)
+      {
         tok.type = TOO_LONG;
         return tok;
-      } else if (isLetter(stream.peek()) ||
-                 isNumber(stream.peek()) ||
-                 isOther(stream.peek())) {
+      }
+      else if (isLetter(stream.peek()) || isNumber(stream.peek()) || isOther(stream.peek()))
+      {
         tok.value[n] = stream.read();
         n++;
-      } else {
+      }
+      else
+      {
         return tok;
       }
     }
-  } else if (stream.peek() == '\r' ||
-             stream.peek() == '\n') {
+  }
+  else if (stream.peek() == '\r' || stream.peek() == '\n')
+  {
     tok.type = EOL;
-    while (stream.available()) {
-      if (n == TOKEN_LEN) {
+    while (stream.available())
+    {
+      if (n == TOKEN_LEN)
+      {
         tok.type = TOO_LONG;
         return tok;
-      } else if (stream.peek() == '\r' ||
-                 stream.peek() == '\n') {
+      }
+      else if (stream.peek() == '\r' || stream.peek() == '\n')
+      {
         tok.value[n] = stream.read();
         n++;
-      } else {
+      }
+      else
+      {
         printf("eol \n");
         return tok;
       }
     }
-  } else { /* unexpected begin of token */
+  }
+  else
+  { /* unexpected begin of token */
     tok.type = UNEXPECTED;
     return tok;
   }
